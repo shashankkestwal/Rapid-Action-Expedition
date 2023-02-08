65073341f1e087882313a6db865771c0b7a4583a
shashankkestwal <shashankkestwal777@gmail.com>
2023-02-08 23:37:41 +0530

    gallery fixed temperory

--- src/ImageGallary/gallery.js
+++ src/ImageGallary/gallery.js
@@ -1,19 +1,44 @@
 import "./gallery.css"
-import img1 from "../assets/img1.png"
-import img2 from "../assets/img2.jpg"
-import img3 from "../assets/img3.jpg"
-import img4 from "../assets/img4.jpg"
-import img5 from "../assets/img5.png"
+import img1 from "../assets/gallery_images/img1.png"
+import img2 from "../assets/gallery_images/img2.png"
+import img3 from "../assets/gallery_images/img3.png"
+import img4 from "../assets/gallery_images/img4.png"
+import img5 from "../assets/gallery_images/img5.png"
+import img6 from "../assets/gallery_images/img6.png"
+import img7 from "../assets/gallery_images/img7.png"
+import img8 from "../assets/gallery_images/img8.png"
+import img9 from "../assets/gallery_images/img9.png"
+import img10 from "../assets/gallery_images/img10.png"
+import img11 from "../assets/gallery_images/img11.png"
+import img12 from "../assets/gallery_images/img12.png"
+import img13 from "../assets/gallery_images/img13.png"
+import img14 from "../assets/gallery_images/img14.png"
+import img15 from "../assets/gallery_images/img15.png"
+import img16 from "../assets/gallery_images/img16.png"
+
+
 import React, { Component } from 'react';
 import Lightbox from 'react-image-lightbox';
 import {gallery_images} from '../data';
 import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
 const images = [
-  img4,
   img1,
-  img3, 
-  img4
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16
 ];
 
 export default class gallery extends Component {
